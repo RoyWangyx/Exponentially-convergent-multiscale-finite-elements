6f3bcb3e8b75cb5fa1ad8b65b084889789cdfef8
Yixuan Wang <33447268+RoyWangyx@users.noreply.github.com>
2026-03-23 16:51:42 -0700

    ok

--- helmholtz/case2 copy/Nodal_can_be_regarded_as_H+bubble_when_N_e=0/agrad.m
+++
@@ -1,6 +0,0 @@
-function a=agrad(t,s)
-%agrad(t,s)=\frac{\partial a(t,s)}{\partial x}=\frac{\partial a(s,t)}{\partial y}
-P = 1.5;
-epsilon = 0.05;
-    a=-2*pi/epsilon*P*cos(2*pi*t/epsilon)/(2 + P*sin(2*pi*t/epsilon))^2/(2 + P*sin(2*pi*s/epsilon));
-end
